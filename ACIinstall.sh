2ba6f3fa7a6890e5e1a8a4a1dbafac212f3f3621
TopOutHC <89717403+TopOutHC@users.noreply.github.com>
2021-08-29 12:52:27 +0900

    Update ACIinstall.sh

--- ACIinstall.sh
+++ ACIinstall.sh
@@ -1,4 +1,5 @@
 # install software
+sudo yum makecache fast
 sudo yum -y install gcc
 sudo yum -y install openssl-devel
 sudo yum -y install libffi-devel
@@ -8,15 +9,18 @@ sudo tar xzf Python-3.7.11.tgz
 cd Python-3.7.11
 sudo ./configure --enable-optimizations
 sudo make altinstall
+cd ..
 # install ansible
-yum install epel-release
-yum install ansible
+yum install -y epel-release
+yum install -y ansible
+cd ..
 #install acitoolkit
 git clone https://github.com/datacenter/acitoolkit.git
 cd acitoolkit
-python setup.py install
+python3.7 setup.py install
+cd ..
 #install Cobra
 wget https://d1nmyq4gcgsfi5.cloudfront.net/fileMedia/1f3d41ce-d154-44e3-74c1-d6cf3b525eaa/acicobra-4.2_3h-py2.py3-none-any.whl
 wget https://d1nmyq4gcgsfi5.cloudfront.net/fileMedia/b3b69aa3-891b-41ff-46db-a73b4b215860/acimodel-4.2_3h-py2.py3-none-any.whl
-pip install acicobra-4.2_3h-py2.py3-none-any.whl
-pip install acimodel-4.2_3h-py2.py3-none-any.whl
+pip3.7 install acicobra-4.2_3h-py2.py3-none-any.whl
+pip3.7 install acimodel-4.2_3h-py2.py3-none-any.whl
