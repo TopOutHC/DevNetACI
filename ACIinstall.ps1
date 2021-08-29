5cb7cf381079b20fe156f4b1b2fa7d2fb3374820
Top Out Human Capital Inc <89717403+TopOutHC@users.noreply.github.com>
2021-08-29 17:18:41 +0900

    Update ACIinstall.ps1

--- ACIinstall.ps1
+++ ACIinstall.ps1
@@ -2,9 +2,6 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
 [Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
 
-#install ACI Learning Labs
-git clone https://github.com/CiscoDevNet/aci-learning-labs-code-samples
-
 # Install Cobra SDK
 Invoke-WebRequest "https://d1nmyq4gcgsfi5.cloudfront.net/fileMedia/1f3d41ce-d154-44e3-74c1-d6cf3b525eaa/acicobra-4.2_3h-py2.py3-none-any.whl" -OutFile "C:\Users\demouser\DevNetACI\acicobra-4.2_3h-py2.py3-none-any.whl"
 Invoke-WebRequest "https://d1nmyq4gcgsfi5.cloudfront.net/fileMedia/b3b69aa3-891b-41ff-46db-a73b4b215860/acimodel-4.2_3h-py2.py3-none-any.whl" -OutFile "C:\Users\demouser\DevNetACI\acimodel-4.2_3h-py2.py3-none-any.whl"
